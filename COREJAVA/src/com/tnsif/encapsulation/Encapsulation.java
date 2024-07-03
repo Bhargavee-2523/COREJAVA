58925fd6efae496a10b254457f24084886b8ae5c
Bhanu.Kiran <Bhanu.Kiran@SBK>
2024-07-03 15:32:30 +0530

    commit message

---
+++ COREJAVA/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialnumber;
+	String name;
+	int age;    //data members
+	void show() {//memebr function
+	System.out.println("serialnumber"+serialnumber+"name"+name+"age"+age);
+		
+	}
+
+}
---
+++ COREJAVA/src/com/tnsif/scanner/Taxcalculation.java
@@ -0,0 +1,20 @@
+package com.tnsif.scanner;
+
+public class Taxcalculation {
+	public static void main(String[] args) {
+		public void calculatetax(Person p)
+		if(p.getIncome()<=1600) {
+			
+		
+			p.setTax(0);
+		}
+	    else if(p.getIncome()>1600 &&p.getIncome()<30000) {
+		p.setTax(5);
+	}	
+	else {
+		p.setTax(10);
+	}
+
+	}	
+
+}
