744703e9933e55a5280ab5994e5c0a1a70497e74
Bhanu.Kiran <Bhanu.Kiran@SBK>
2024-07-03 13:59:54 +0530

    commit message

---
+++ COREJAVA/src/com/tnsif/packages/PublicDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.packages;
+
+public class PublicDemo {
+	protected int a=8;
+	public static void main(String[] args) {
+		PublicDemo p=new PublicDemo();
+		System.out.println(p.a);
+		
+	}
+
+}
---
+++ COREJAVA/src/com/tnsif/scanner/ScannerExample.java
@@ -0,0 +1,13 @@
+package com.tnsif.scanner;
+import java.util.Scanner;
+public class ScannerExample {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your name");
+		String name=sc.nextLine();
+		System.out.println("enter roll number");
+		int roll=sc.nextInt();
+	}
+
+}
+
